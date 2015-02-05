76c2a7d43f06824087ead0957571656736dee55d
Rohan Singh <rohandhaka24@gmail.com>
2015-02-05 18:08:56 +0530

    Added sorting function for merged array

--- commonSubstring/commonSubstring.java
+++ commonSubstring/commonSubstring.java
@@ -1,36 +1,67 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 
 public class commonSubstring {
-	
-	 public static void main(String[] args) throws IOException
-	    {    
-		 
-		 	commonSubstring obj = new commonSubstring();
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        System.out.println("Longest Common Substring Algorithm Test\n");
-	 
-	        System.out.println("\nEnter string 1");
-	        String str1 = br.readLine();
-	 
-	        System.out.println("\nEnter string 2");
-	        String str2 = br.readLine();
-	        
-	        SuffixArray suffixarray1 = new SuffixArray(str1);
-	        suffixarray1.createSuffixArray();	        
-	        obj.store(suffixarray1.getsuffix(), suffixarray1.getindex(),"1", str1);
-	        
-	        SuffixArray suffixarray2 = new SuffixArray(str1);
-	        suffixarray2.createSuffixArray();
-	        obj.store(suffixarray2.getsuffix(), suffixarray2.getindex(),"2", str2);
-	        
-	        //LongestSubstring obj = new LongestSubstring(); 
-	        //String result = bstring(str1, str2);
-	 
-	        //System.out.println("\nLongest Common Substring : "+ result);
-	    }
+    
+     public static void main(String[] args) throws IOException
+        {    
+         
+            commonSubstring obj = new commonSubstring();
+            System.out.println("Longest Common Substring Algorithm Test\n");
+            Scanner in=new Scanner(System.in);
+            
+            System.out.println("\nEnter string 1");
+            String str1 ="";
+            while(in.hasNextLine())
+            {
+                String str=in.nextLine();
+                if(str.equals("end"))
+                    break;
+                str1=str1+str;
+            }
+            str1.toLowerCase();
+            //System.out.println(str1);
+     
+            System.out.println("\nEnter string 2");
+            String str2 ="";
+            while(in.hasNextLine())
+            {
+                String str="";
+                str=in.nextLine();
+                if(str.equals("end"))
+                    break;
+                str2=str2+str;
+            }
+            str2.toLowerCase();
+            in.close();
+            SuffixArray suffixarray1 = new SuffixArray(str1);
+            suffixarray1.createSuffixArray();           
+            String suffix1[][]=obj.store(suffixarray1.getsuffix(), suffixarray1.getindex(),"1");
+            
+            SuffixArray suffixarray2 = new SuffixArray(str2);
+            suffixarray2.createSuffixArray();
+            String suffix2[][]=obj.store(suffixarray2.getsuffix(), suffixarray2.getindex(),"2");
+            
+            String suffix[][]=obj.merge(suffix1, suffix2);
+            
+            Arrays.sort(suffix, new ColumnComparator(0));           
+            for(int i=0;i<suffix.length;i++)
+            {
+                System.out.println();
+                for(int j=0;j<4;j++)
+                {
+                    System.out.print(suffix[i][j]+" ");
+                }
+            }
+            
+            //LongestSubstring obj = new LongestSubstring(); 
+            //String result = bstring(str1, str2);
+     
+            //System.out.println("\nLongest Common Substring : "+ result);
+        }
      public String[][] merge(String art1[][], String art2[][])
         {
             String article[][]=new String[art1.length+art2.length][4];
@@ -46,28 +77,41 @@ public class commonSubstring {
             {
                 for(int j=0;j<4;j++)
                 {
-                    article[i][j]=art2[k][j];
-                    i++;
+                    article[i][j]=art2[k][j];                    
                 }
+                i++;
             }
             return article;     
         }
-	 
-	 public void store(String[] suffixarr, int[] index, String doc, String str)
-	 {
-		 String[][] arr = new String[suffixarr.length][4];
-		 
-		 for(int i=0;i<suffixarr.length;i++)
-		 {
-			 arr[i][0] = suffixarr[i];			
-			 arr[i][1] = ""+ index[i];
-			 arr[i][2] = ""+ doc;
-			 arr[i][3] = ""+ suffixarr[i].length();			 
-		 }			 
-	 }	 
+     
+     public String[][] store(String[] suffixarr, int[] index, String doc)
+     {
+         String[][] arr = new String[suffixarr.length][4];
+         
+         for(int i=0;i<suffixarr.length;i++)
+         {
+             arr[i][0] = suffixarr[i];          
+             arr[i][1] = ""+ index[i];
+             arr[i][2] = ""+ doc;
+             arr[i][3] = ""+ suffixarr[i].length();          
+         }
+         return arr;
+     }   
 
 }
-
+class ColumnComparator implements Comparator {
+    int columnToSort;
+    ColumnComparator(int columnToSort) {
+        this.columnToSort = columnToSort;
+    }
+    //overriding compare method
+    public int compare(Object o1, Object o2) {
+        String[] row1 = (String[]) o1;
+        String[] row2 = (String[]) o2;
+        //compare the columns to sort
+        return row1[columnToSort].compareTo(row2[columnToSort]);
+    }
+}
 class SuffixArray
 {
     private String[] text;
@@ -89,14 +133,14 @@ class SuffixArray
         for (int i = 0; i < length; i++)
         {
             index[i] = i;
-        }	
+        }   
  
         suffix = new String[length];
     }
  
     public void createSuffixArray()
     {   
-        for(int index = 0; index < length; index++)	
+        for(int index = 0; index < length; index++) 
         {
             String text = "";
             for (int text_index = index; text_index < length; text_index++)
@@ -132,19 +176,19 @@ class SuffixArray
  
        /* System.out.println("SUFFIX \t INDEX");
         for (int iterate = 0; iterate < length; iterate++)
-        {		
+        {       
             System.out.println(suffix[iterate] + "\t" + index[iterate]);
         }*/
     }
     
     public String[] getsuffix()
     {
-    	return suffix;
+        return suffix;
     }
     
     public int[] getindex()
     {
-    	return index;
+        return index;
     }
  
  
@@ -157,5 +201,5 @@ class SuffixArray
  
         SuffixArray suffixarray = new SuffixArray(text);
         suffixarray.createSuffixArray();
-    }	*/
+    }   */
 }
